62f0b7768f49f0ea9f6d38f51fba42bc6886e932
AllHailTheFox <ervynpang@yahoo.com>
2022-05-31 22:44:14 +0800

    Worker file

--- worker.py
+++ worker.py
@@ -5,8 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = redis.from_url(os.environ.get("REDIS_URL"))
-conn = redis.from_url(redis_url)
+conn = redis.from_url(os.environ.get("REDIS_URL"))
 
 if __name__ == '__main__':
     with Connection(conn):
