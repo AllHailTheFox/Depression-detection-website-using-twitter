f86959eb1b62b6587959a86d6540eb5b94b24274
AllHailTheFox <ervynpang@yahoo.com>
2022-05-31 21:58:51 +0800

    Worker file

--- worker.py
+++ worker.py
@@ -5,9 +5,9 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'https://rtdetectiondepression.herokuapp.com/')
+REDIS_DEFAULT_CONNECTION_POOL = redis.ConnectionPool.from_url(os.getenv('REDIS_URL', 'redis://localhost:6379/'))
 
-conn = redis.from_url(redis_url)
+conn = redis.from_url(REDIS_DEFAULT_CONNECTION_POOL)
 
 if __name__ == '__main__':
     with Connection(conn):
